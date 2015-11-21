7bf916fc69a1ef6cffe54af61a161e7909533a5d
Huh <Joshnwk@Yahoo.com>
2015-11-20 18:14:23 -0700

    First draft of actual interface
    UI functions, but does not connect to DB and the second tab is useless
    for displaying results.

--- server.R
+++ server.R
@@ -1,58 +1,147 @@
 library(shiny)
 library(ggplot2)
 
+estBetaParams <- function(mu, var) {
+    alpha <- mu * (((mu * (1 - mu))/var) - 1)
+    beta <- (1 - mu) * (((mu * (1 - mu))/var) - 1)
+    return(params = list(alpha = alpha, beta = beta))
+}
+
+iter <- 0
+
 # Define server logic for random distribution application
-shinyServer(function(input, output, session) {
+shinyServer(function(input, output, session){
 
-    observeEvent(input$expert, {
-        if(input$expert == "Low"){
-            updateSliderInput(session, "mu_slider", label = "Lower Boundary",
-                min = 0, max = .9, value = .5, step = .1)
-            updateSliderInput(session, "sd_slider", label = "Upper Boundary",
-                min = 0.1, max = 1, value = .5, step = .1)
-        }
-        if(input$expert == "High"){
-            updateSliderInput(session, "mu_slider", label = "Mean", min = 0.1,
-                max = 0.99, value = .5, step = 0.1)
-            updateSliderInput(session, "sd_slider", label = "Uncertainty",
-                min = 0.01, max = 0.99, value = 0.5, step = 0.1)
+    #  Observe mean slider and update SD slider to appropriate range when mu
+    #  changes
+    observeEvent(input$mu_slider, {
+        if(input$distr_type == "Continuous"){
+            cond_var <- input$mu_slider * (1 - input$mu_slider)
+            updateSliderInput(session, "sd_slider", "Uncertainty",
+                min = 0, max = cond_var - 0.01, value = 0.01,
+                step = 0.005)
         }
     })
 
-    #  Container to hold data
-    dat <- reactive({
-        if(input$expert == "Low"){
-            out <- data.frame(val = runif(10000, input$mu_slider,
-                input$sd_slider))
-        }
-        if(input$expert == "High"){
-            out <- data.frame(val = rnorm(10000, mean = input$mu_slider,
-                sd = input$sd_slider))
-        }
-    return(out)
-    })
+    #  Create container to hold data because it is used in at least two places
+    dat_in <- reactiveValues()
 
-    output$plot <- renderPlot({
-        ggplot(dat(), aes(x = val)) +
-            geom_density(alpha = .4, fill = "dodgerblue",
-                colour = "dodgerblue") +
+    output$cont_plot <- renderPlot({
+        #  Estimate beta parameters from input sliders
+        beta_parms <- estBetaParams(input$mu_slider, input$sd_slider)
+
+        #  Assign parameters to data container for later
+        dat_in$parms <- data.frame(
+            id = input$userid,
+            disease = input$disease,
+            scenario = input$scenario,
+            name = ifelse(is.null(input$ds_name), NA, input$ds_name),
+            facilitator = input$facilitator,
+            alpha = beta_parms$alpha,
+            beta = beta_parms$beta)
+
+        #  Create data frame of 10,000 random draws from beta distribution
+        #  parameterized by slider values, but this will only occur when
+        #  the distribution choice is continuous.  Data is stored in reactive
+        #  object to make it available later.
+        dat_in$dat <- data.frame(val = rbeta(30000, beta_parms$alpha,
+            beta_parms$beta))
+
+        #  Plot the resulting data so the user can see it and respond
+        ggplot(dat_in$dat, aes(x = val)) +
+            geom_density(fill = rgb(0, 132, 204, 200, maxColorValue = 255),
+                colour = NA) +
             theme_bw() +
-            xlab("Risk") +
+            xlab("") +
+            ylab("") +
             theme(legend.position = "none") +
 			theme(panel.border = element_blank(),
 			    axis.line = element_line(color = "black"))
+    })
+
+    #  Generate a user input number of boxes for discrete analysis
+    output$g_boxes1 <- renderUI({
+        fluidRow(
+            lapply(1:ifelse(input$n_groups <= 6, input$n_groups, 6), function(i){
+                column(floor(12/ifelse(input$n_groups <= 6, input$n_groups, 6)),
+                    textInput(paste0("txt", i), paste("Group", i),
+                        width = "80px", value = ifelse(i == 1, 20, 0))
+                )
+            })
+        )
+    })
+
+    output$g_boxes2 <- renderUI({
+        if(input$n_groups > 6){
+            tmp <- input$n_groups - 6
+            fluidRow(
+                lapply(7:(6 + tmp), function(i){
+                    column(floor(10/tmp),
+                        textInput(paste0("txt", i), paste("Group", i),
+                            width = "80px", value = ifelse(i == 1, 20, 0))
+                    )
+                })
+            )
+        }
+    })
+
+    output$sum20 <- renderText({
+        if(is.null(input$n_groups))
+            return()
+
+        #  Recover user inputs
+        user_vals <- numeric(as.numeric(input$n_groups))
+
+        for(i in 1:length(user_vals)){
+            user_vals[i] <- as.numeric(input[[paste0("txt", i)]])
+        }
+
+        sum(user_vals)
 
     })
 
+    #  Generate discrete data and plot
+    output$disc_plot <- renderPlot({
+        if(is.null(input$n_groups))
+            return()
+
+        #  Recover user inputs
+        user_vals <- numeric(as.numeric(input$n_groups))
+
+        for(i in 1:length(user_vals)){
+            user_vals[i] <- as.numeric(input[[paste0("txt", i)]])
+        }
+
+        #  Create data
+        dat <- data.frame(vals = sample(1:length(user_vals), 30000,
+            prob = user_vals, replace = T))
+
+        ggplot(dat, aes(x = vals)) +
+            geom_bar(fill = rgb(0, 132, 204, 200, maxColorValue = 255),
+                binwidth = 1, origin = -0.5) +
+            theme_bw() +
+            xlab("") +
+            ylab("Count") +
+            theme(legend.position = "none") +
+			theme(panel.border = element_blank(),
+			    axis.line = element_line(color = "black")) +
+            scale_x_continuous(breaks = 0:length(user_vals),
+                limits = c(0.5, (as.numeric(input$n_groups) + 0.5)))
+
+    })
+
+    #  Update text that reports the quantiles
     output$min_val <- renderText({
-        paste("Minimum value likely to occur =",
-            round(quantile(dat()$val, 0.025), 2))
+        paste("Lower probable bound =",
+            round(quantile(dat_in$dat$val, 0.025), 2))
     })
     output$mean_val <- renderText({
-        paste("Most likely value =", input$mu_slider)
+        paste("Most likely value =", round(quantile(dat_in$dat$val, 0.5), 2))
     })
     output$max_val <- renderText({
-        paste("Maximum value likely to occur =",
-            round(quantile(dat()$val, 0.975), 2))
+        paste("Upper probable bound =",
+            round(quantile(dat_in$dat$val, 0.975), 2))
     })
+
+
 })
--- ui.R
+++ ui.R
@@ -2,42 +2,123 @@ library(shiny)
 
 # Define UI for random distribution application
 shinyUI(fluidPage(
+  tags$head(
+    tags$style(HTML('#fitgo{background-color:#0044CC;
+                        color:white;
+                        margin-top:40px}'))
+  ),
 
   # Application title
-  titlePanel("Risk Tolerance"),
+  headerPanel(a(h2("Expert Opinion", style = "color:black"),
+        a(href = "http://popr.cfc.umt.edu/",
+            h4("Powered by PopR")))),
 
   # Sidebar with controls to select the random distribution type
   # and number of observations to generate. Note the use of the
   # br() element to introduce extra vertical spacing
   sidebarLayout(
     sidebarPanel(
-        textInput("userid", "Last Name", "Enter Last Name", width = "100%"),
-        textInput("org", "Organization", "Provide Affiliation", width = "100%")
+        tags$div(title = "Use this box to choose the user number that the
+            facilitator assigned you at the beginning of the session",
+            selectInput("userid", "Your User Number", choices = 1:50)
+        ),
+        tags$div(title = "Use this box to set the disease number",
+            selectInput("disease", "Disease Number", choices = 1:50)
+        ),
+        tags$div(title = "Use this box to set the current scenario number",
+            selectInput("scenario", "Scenario Number", choices = 1:50)
+        ),
+        tags$div(title = "You may use this box to give the disease by scenario
+            combination a unique name, but this information is optional.",
+            textInput("ds_name", "Scenario Name (Optional)")
+        ),
+        tags$div(title = "Use this box to set the facilitator's number",
+            selectInput("facilitator", "Facilitator Number", choices = 1:10)
+        ),
+        tags$div(title = "Use this box to select the type of distribution to use
+            for a scenario.  The facilitator will provide guidance on how to
+            make this selection.",
+            selectInput("distr_type", "Distribution Type",
+                choices = list("Continuous", "Discrete"))
+        )
     ),
 
     # Show a tabset that includes a plot, summary, and table view
     # of the generated distribution
     mainPanel(
-        selectInput("param", "Select Parameter",
-            choices = c("Survival", "Extinction")
-        ),
-        radioButtons("expert", "Level of Expertise",
-            choices = c("Low", "High"), selected = "High"
-        ),
-        plotOutput("plot"),
-        fluidRow(
-            column(6,
-                sliderInput("mu_slider", "Mean", min = 0, max = 1, step = 0.1,
-                    value = 0.5)
+        tabsetPanel(
+            tabPanel("Setup",
+                conditionalPanel("input.distr_type == 'Continuous'",
+                    plotOutput("cont_plot"),
+                    fluidRow(
+                        column(6,
+                            sliderInput("mu_slider", "Expected Value",
+                                min = 0.01, max = 0.99, value = 0.5, step = 0.1,
+                                width = "100%")
+                        ),
+                        column(6,
+                            sliderInput("sd_slider", "Uncertainty",
+                                min = 0, max = 0.25, value = 0.01, step = 0.1,
+                                width = "100%")
+                        )
+                    ),
+                    fluidRow(
+                        column(4, h6(textOutput("min_val"))),
+                        column(4, h6(textOutput("mean_val"))),
+                        column(4, h6(textOutput("max_val")))
+                    )
+                ),
+                conditionalPanel("input.distr_type == 'Discrete'",
+                    sliderInput("n_groups", "Number of Groups", min = 1,
+                        max = 10, value = 2, step = 1, width = "100%"),
+                    uiOutput("g_boxes1"),
+                    conditionalPanel("input.n_groups > 6",
+                        uiOutput("g_boxes2")
+                    ),
+                    hr(),
+                    h5("The sum of the groups must equal 20!",
+                        style = "color:red"),
+                    fluidRow(
+                        column(6, h4("Current Groups Sum")),
+                        column(6, h4(textOutput("sum20")))
+                    ),
+                    hr(),
+                    plotOutput("disc_plot")
+                ),
+                tags$div(title = "Click here to submit your opinion",
+                    shiny::actionButton("fitgo", "Submit",
+                        width = "100%",
+                        icon = icon("exchange")),
+                    style = "padding-bottom:200px")
+
             ),
-            column(6,
-                sliderInput("sd_slider", "Uncertainty", min = 0.0001, max = 1,
-                    step = 0.01, value = 1)
+            tabPanel("My Results",
+                fluidRow(
+                    column(4,
+                        tags$div(title = "If you provided a unique name for
+                            each scenario you can use this box to display the
+                            results of those excercises.",
+                            selectInput("ds_name_recall", "Scenario Name",
+                                choices = NULL)
+                        )
+                    ),
+                    column(4,
+                        tags$div(title = "Choose the disease number you would
+                            like to display",
+                            selectInput("disease_recall", "Disease Number",
+                                choices = 1:50)
+                        )
+                    ),
+                    column(4,
+                        tags$div(title = "Choose the scenario number you would
+                            like to display",
+                            selectInput("scenario_recall", "Scenario Number",
+                                choices = 1:50)
+                        )
+                    )
+                )
             )
-        ),
-        h6(textOutput("min_val")),
-        h6(textOutput("mean_val")),
-        h6(textOutput("max_val"))
+        )
     )
   )
 ))
